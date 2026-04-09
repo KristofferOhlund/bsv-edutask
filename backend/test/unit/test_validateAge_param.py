564fbe96a2800a1bf2bd9aa06aeb4a13a5bdac87
SIMON-5000 <sidr24@student.bth.com>
2026-04-09 12:04:37 +0200

    update fixture and parametrization

--- backend/test/unit/test_validateAge_param.py
+++ backend/test/unit/test_validateAge_param.py
@@ -3,24 +3,13 @@ from src.util.helpers import ValidationHelper
 from unittest.mock import MagicMock
 
 
-@pytest.fixture(params=["age"])
-def mock_factory(request):
+@pytest.fixture()
+def mock_helper(age):
     mock_user = MagicMock() # Mock object / class
-    mock_user.get = MagicMock(return_value={"age": request.param}) # mock function with return value
+    mock_user.get = MagicMock(return_value={"age": age}) # mock function with return value
     validation_helper = ValidationHelper(mock_user)
-    print("PARAMETER", request.params, "\n\n")
+    print("\nPARAMETER", age, "\n")
     return validation_helper
-
-
-# @pytest.fixture
-# def helper_factory():
-#     # private, only used through parent.
-#     def _validation_helper_creator(3):
-#         mock_user = MagicMock()
-#         mock_user.get = MagicMock(return_value={"age": age})
-#         validation_helper = ValidationHelper(mock_user)
-#         return validation_helper
-#     return _validation_helper_creator
 
 
 @pytest.fixture
@@ -30,6 +19,7 @@ def userid():
 
 @pytest.mark.param
 @pytest.mark.parametrize("age,expected", [(-1, "invalid"), (0, "underaged"), (1, "underaged"), (17, "underaged"), (18, "valid"), (19, "valid"), (119, "valid"), (120, "valid"), (121, "invalid")])
-def test_validateAge(age, expected, userid, mock_factory):
-    # helper = mock_factory(age)
-    assert mock_factory.validateAge(userid) == expected
+def test_validateAge(expected, mock_helper, userid):
+    print("\n\n", type(mock_helper))
+    result = mock_helper.validateAge(userid)
+    assert result == expected
