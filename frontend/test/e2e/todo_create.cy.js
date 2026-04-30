dd4001e02394e0e52384a54ef8204870c6a41221
Kristoffer <kristoffer.ohlund@icloud.com>
2026-04-30 14:17:08 +0200

    first commit

---
+++ frontend/test/e2e/todo_create.cy.js
@@ -0,0 +1,12 @@
+describe("Todo create", () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:3000/");
+        cy.get("#email").type("kroh24@student.bth.se{enter}");
+    })
+    // it("Create Task", () => {
+    // // make sure the landing page contains a header with "login"
+    // cy.get("#title").type("cool title{enter}");
+    // });
+
+    // create Todo
+})
