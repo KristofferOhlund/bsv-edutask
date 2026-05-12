b9e30e058e9001f01f372a1db38fd1696f92c4bd
Kristoffer <kristoffer.ohlund@icloud.com>
2026-05-12 12:03:47 +0200

    GUI testing

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -5,7 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000'
+    baseUrl: "http://localhost:5000",
+    specPattern: "test/e2e/**/*.cy.js",
+    supportFile: false,
   },
 
   component: {
--- frontend/test/e2e/todo_create.cy.js
+++ frontend/test/e2e/todo_create.cy.js
@@ -3,10 +3,16 @@ describe("Todo create", () => {
         cy.visit("http://localhost:3000/");
         cy.get("#email").type("kroh24@student.bth.se{enter}");
     })
-    // it("Create Task", () => {
-    // // make sure the landing page contains a header with "login"
-    // cy.get("#title").type("cool title{enter}");
-    // });
+    
+    it("Create Todo item", () => {
+    // Select previous created Task
+    cy.get('.container .container-element').first().click()
+    cy.get('[placeholder="Add a new todo item"]').click().type("Our first todo{enter}")
+    })
 
+    it("Toggle Todo item", () => {
+        cy.get(".container .container-element").first().click();
+        cy.get('.unchecked').first().click()
+    })
     // create Todo
 })
