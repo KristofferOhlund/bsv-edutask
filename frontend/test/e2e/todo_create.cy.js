1f92c122807cc104bb06dc042993b939dba32b05
Kristoffer <kristoffer.ohlund@icloud.com>
2026-05-12 15:58:34 +0200

    Update delete item to force rerender

--- frontend/test/e2e/todo_create.cy.js
+++ frontend/test/e2e/todo_create.cy.js
@@ -3,22 +3,25 @@ describe("Todo tests", () => {
         cy.visit("http://localhost:3000/");
         cy.get("#email").type("kroh24@student.bth.se{enter}");
         cy.viewport(2000, 2000);
+        cy.get(".container-element:has(a)").first().click();
     })
     
-    // it("Create Todo item", () => {
-    // // Select previous created Task
-    //   cy.get('.container .container-element').first().click()
-    //   cy.get('[placeholder="Add a new todo item"]').click().type("Our first todo{enter}")
-    // })
+    it("Create Todo item", () => {
+    //   cy.get(".container-element:has(a)").first().click();
+      cy.get('[placeholder="Add a new todo item"]').click().type("Our first todo{enter}")
+      cy.contains(".todo-item", "Our first todo").parent().find(".remover").first().click();
+    })
 
     it("Toggle Todo item", () => {
       // Arrange
-      cy.contains('.title-overlay', 'Test task').click()
       cy.get('[placeholder="Add a new todo item"]').click().type("A new todo_1{enter}")
+      const checker = cy
+        .contains(".todo-item", "A new todo_1")
+        .find(".checker");
+      
       // Act
-      // cy.get(".container .container-element").first().click();
-      const checker = cy.contains('.todo-item', "A new todo_1").find('.checker');
       checker.click();
+
       // Assert toggle on and off (double asserts OK? separate tests?)
       checker.should('have.class', 'checked');
       checker.click();
@@ -28,4 +31,16 @@ describe("Todo tests", () => {
     })
 
     // Delete Todo
+    it("Delete todo item", () => {
+        cy.get('[placeholder="Add a new todo item"]')
+          .click()
+          .type("A new todo_1{enter}");
+        cy.get(".todo-list").find("li").first().children(".remover").click();
+        cy.get(".close-btn").click()
+        cy.get(".container-element:has(a)").first().click();
+        cy.contains(".todo-item", "A new todo_1").should("not.exist");
+
+        // Nu är GUI uppdaterat
+        cy.get(".todo-item").should("have.length", 0);
+    })
 })
