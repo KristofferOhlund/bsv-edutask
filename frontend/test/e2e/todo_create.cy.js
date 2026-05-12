5a237a891bdcf32ded738412be05d645bbb50be4
SIMON-5000 <sidr24@student.bth.com>
2026-05-12 14:29:39 +0200

    add toggle test

--- frontend/test/e2e/todo_create.cy.js
+++ frontend/test/e2e/todo_create.cy.js
@@ -1,18 +1,31 @@
-describe("Todo create", () => {
+describe("Todo tests", () => {
     beforeEach(() => {
         cy.visit("http://localhost:3000/");
         cy.get("#email").type("kroh24@student.bth.se{enter}");
+        cy.viewport(2000, 2000);
     })
     
-    it("Create Todo item", () => {
-    // Select previous created Task
-    cy.get('.container .container-element').first().click()
-    cy.get('[placeholder="Add a new todo item"]').click().type("Our first todo{enter}")
-    })
+    // it("Create Todo item", () => {
+    // // Select previous created Task
+    //   cy.get('.container .container-element').first().click()
+    //   cy.get('[placeholder="Add a new todo item"]').click().type("Our first todo{enter}")
+    // })
 
     it("Toggle Todo item", () => {
-        cy.get(".container .container-element").first().click();
-        cy.get('.unchecked').first().click()
+      // Arrange
+      cy.contains('.title-overlay', 'Test task').click()
+      cy.get('[placeholder="Add a new todo item"]').click().type("A new todo_1{enter}")
+      // Act
+      // cy.get(".container .container-element").first().click();
+      const checker = cy.contains('.todo-item', "A new todo_1").find('.checker');
+      checker.click();
+      // Assert toggle on and off (double asserts OK? separate tests?)
+      checker.should('have.class', 'checked');
+      checker.click();
+      checker.should('have.class', 'unchecked');
+      // Clean-up
+      checker.parent().find('.remover').click();
     })
-    // create Todo
+
+    // Delete Todo
 })
