c3a6580ff219224e505ccdad23183a2df4ab3c35
Kristoffer <kristoffer.ohlund@icloud.com>
2026-05-16 13:43:26 +0200

    Add intercepts and test is disabled on empty input test

--- frontend/test/e2e/test_todos.cy.js
+++ frontend/test/e2e/test_todos.cy.js
@@ -1,45 +1,96 @@
 describe("Todo tests", () => {
     beforeEach(() => {
-        cy.visit("http://localhost:3000/");
-        cy.get("#email").type("kroh24@student.bth.se{enter}");
-        cy.viewport(2000, 2000);
-        cy.get(".container-element:has(a)").first().click();
+      // To create waiters for updates in DOM
+      // https://docs.cypress.io/api/commands/wait
+      cy.intercept('GET', '**/users/**').as('getUser');
+      cy.intercept('GET', '**/tasks/**').as('getTaskTodos');
+      cy.intercept('POST', '**/todos/**').as('createTodo');
+      cy.intercept('DELETE', '**/todos/**').as('deleteTodo');
+
+      cy.viewport(1200, 3000);
+      cy.visit("http://localhost:3000/");
+      cy.get("#email").type("kroh24@student.bth.se{enter}");
+      cy.wait('@getUser');
+
+      cy.contains(".container-element", "Test task").click();
+      cy.wait('@getTaskTodos');
+      // https://docs.cypress.io/api/commands/each#DOM-Elements
+
+      removeTodos();
     })
-    
-    it("Create Todo item", () => {
+
+    // .each() breaks, https://stackoverflow.com/a/69371000 is flaky, 
+    function removeTodos() {
+      cy.get('[placeholder="Add a new todo item"]').should('be.visible');
+      cy.get('.todo-list').then(($todoList) => {
+        if($todoList.find('.remover').length) {
+          cy.get('.remover').first().click();
+          // Wait for mongo calls
+          cy.wait('@deleteTodo');
+          cy.wait('@getTaskTodos');
+          removeTodos();
+        }
+      })
+    }
+
+    it("Create named Todo item", () => {
+      // Cont. Arrange
+      cy.get('[placeholder="Add a new todo item"]').should('be.visible');
+      
+      // Act
       cy.get('[placeholder="Add a new todo item"]').click().type("Our first todo{enter}");
-      cy.contains(".todo-item", "Our first todo").should("exist");
-      cy.contains(".todo-item", "Our first todo").parent().find(".remover").first().click();
+      // Wait for mongo. Create todo, Fetch todos.
+      cy.wait('@createTodo');
+      cy.wait('@getTaskTodos');
+      
+      // Assert
+      cy.contains(".todo-item", "Our first todo").should("be.visible");
+
+      // Clean-up
+      cy.contains(".todo-item", "Our first todo").find(".remover").click();
+      cy.wait('@deleteTodo');
+      cy.wait('@getTaskTodos');
+    })
+
+    it("Button is disabled on empty input", () => {
+      cy.get('.todo-list').find('input[type="submit"]').should('be.disabled');
+      cy.get('.todo-list').find('input[type="submit"]').should('have.attr', 'disabled');
+    })
+
+    it("Button not disabled with text in input", () => {
+      cy.get('[placeholder="Add a new todo item"]').click().type("Our first todo");
+      cy.get('.todo-list').find('input[type="submit"]').should('not.be.disabled');
+      cy.get('.todo-list').find('input[type="submit"]').should('not.have.attr', 'disabled');
     })
 
     it("Toggle Todo item", () => {
       // Arrange
+      cy.get('[placeholder="Add a new todo item"]').should('be.visible');
       cy.get('[placeholder="Add a new todo item"]').click().type("A new todo_1{enter}")
-      const checker = cy
-        .contains(".todo-item", "A new todo_1")
-        .find(".checker");
-      
-      // Act
-      checker.click();
+      cy.wait('@createTodo');
+      cy.wait('@getTaskTodos');
 
-      // Assert toggle on and off (double asserts OK? separate tests?)
-      checker.should('have.class', 'checked');
-      checker.click();
-      checker.should('have.class', 'unchecked');
-      // Clean-up
-      checker.parent().find('.remover').click();
+      // Act
+      cy.contains(".todo-item", "A new todo_1").find(".checker").click();
+
+      // Assert toggle on and off (double asserts OK?)
+      cy.contains(".todo-item", "A new todo_1").find(".checker").should('have.class', 'checked');
+      cy.contains(".todo-item", "A new todo_1").find(".checker").click();
+      cy.contains(".todo-item", "A new todo_1").find(".checker").should('have.class', 'unchecked');
     })
 
     // Delete Todo
     it("Delete todo item", () => {
-        cy.get('[placeholder="Add a new todo item"]')
-          .click()
-          .type("A new todo_1{enter}");
-        cy.get(".todo-list").find("li").first().children(".remover").click();
-        cy.get(".close-btn").click()
-        cy.get(".container-element:has(a)").first().click();
+      cy.get('[placeholder="Add a new todo item"]').should('be.visible');
+      cy.get('[placeholder="Add a new todo item"]').click().type("A new todo_2{enter}");
 
-        // Nu är GUI uppdaterat
-        cy.get(".todo-list").should("have.length", 1);
+      cy.wait('@createTodo');
+      cy.wait('@getTaskTodos');
+
+      cy.contains(".todo-item", "A new todo_2").find(".remover").click();
+      
+      cy.wait('@deleteTodo');
+      cy.wait('@getTaskTodos');
+      cy.contains(".todo-item", "A new todo_2").should('not.exist');
     })
 })
